bcb7c18010c3c98d7c208dba17046177312e7389
Michaël Cloots <michael.cloots@thomasmore.be>
2021-11-26 14:13:26 +0100

    Seeder

--- EfCoreTest/Program.cs
+++ EfCoreTest/Program.cs
@@ -12,6 +12,9 @@ namespace EfCoreTest
         {
             using ShopContext context = new ShopContext();
 
+            Seeder seeder = new Seeder(context);
+            seeder.Seed();
+
             var shopService = new ShopService(context);
             //var products = shopService.GetProducts();
             //var products = shopService.GetProductsQuery().Where(p => p.Price >= 10.00M).OrderBy(p => p.Name);
@@ -45,112 +48,7 @@ namespace EfCoreTest
                 Console.WriteLine($"Total price: {Math.Round(shopService.GetTotalPriceOrder(o.Id),2)}");
             }
 
-            //ProductOrder po = new ProductOrder()
-            //{
-            //    OrderId = 1,
-            //    ProductId = 1,
-            //    Quantity = 5
-            //};
-
-            //ProductOrder po2 = new ProductOrder()
-            //{
-            //    OrderId = 2,
-            //    ProductId = 2,
-            //    Quantity = 2
-            //};
-
-            //ProductOrder po3 = new ProductOrder()
-            //{
-            //    OrderId = 1,
-            //    ProductId = 2,
-            //    Quantity = 10
-            //};
-
-            //context.Add(po);
-            //context.Add(po2);
-            //context.Add(po3);
-            //context.SaveChanges();
-
-
-            //Customer customerMichael = new Customer()
-            //{
-            //    FirstName = "Michaël",
-            //    LastName = "Cloots",
-            //    Email = "michael.cloots@thomasmore.be"
-            //};
-
-            //Customer customerJos = new Customer()
-            //{
-            //    FirstName = "Jos",
-            //    LastName = "Jossen",
-            //    Email = "jos.jossen@thomasmore.be"
-            //};
-
-            //context.Add(customerMichael);
-            //context.Add(customerJos);
-
-            //context.SaveChanges();
-
-            //Order order = new Order()
-            //{
-            //    OrderPlaced = DateTime.Now,
-            //    CustomerId = 1
-            //};
-
-            //Order order2 = new Order()
-            //{
-            //    OrderPlaced = DateTime.Now.AddDays(-7),
-            //    CustomerId = 1
-            //};
-
-            //context.Add(order);
-            //context.Add(order2);
-            //context.SaveChanges();
-
-
-
-
-            //var products = context.Products
-            //    .Where(p => p.Price >= 5.00M)
-            //    .OrderBy(p => p.Name);
-
-            //var productsLinq = from product in context.Products
-            //                   where product.Price >= 5.00M
-            //                   orderby product.Name
-            //                   select product;
-
-            //foreach (var p in products)
-            //{
-            //    Console.WriteLine($"Id: {p.Id}");
-            //    Console.WriteLine($"Name: {p.Name}");
-            //    Console.WriteLine($"Price: {p.Price}");
-            //    Console.WriteLine(new string('-',20));
-            //}
-
             Console.ReadKey();
-
-            //Product woodyPajamas = new Product()
-            //{
-            //    Name = "Red Woody pajama",
-            //    Price = 15.99M
-            //};
-            //context.Products.Add(woodyPajamas);
-
-            //Product redCap = new Product()
-            //{
-            //    Name = "Red cap",
-            //    Price = 8.99M
-            //};
-            //context.Add(redCap);
-
-            //Product blueTshirt = new Product()
-            //{
-            //    Name = "Blue t-shirt",
-            //    Price = 19.99M
-            //};
-            //context.Add(blueTshirt);
-
-            //context.SaveChanges();
         }
     }
 }
---
+++ EfCoreTest/Services/Seeder.cs
@@ -0,0 +1,106 @@
+﻿using EfCoreTest.Data;
+using EfCoreTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCoreTest.Services
+{
+    public class Seeder
+    {
+        private readonly ShopContext _context;
+        public Seeder(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (!_context.Products.Any())
+            {
+                //Seeding data
+                Customer customerMichael = new Customer()
+                {
+                    FirstName = "Michaël",
+                    LastName = "Cloots",
+                    Email = "michael.cloots@thomasmore.be"
+                };
+
+                Customer customerJos = new Customer()
+                {
+                    FirstName = "Jos",
+                    LastName = "Jossen",
+                    Email = "jos.jossen@thomasmore.be"
+                };
+
+                _context.Add(customerMichael);
+                _context.Add(customerJos);
+
+                Product woodyPajamas = new Product()
+                {
+                    Name = "Red Woody pajama",
+                    Price = 15.99M
+                };
+                _context.Products.Add(woodyPajamas);
+
+                Product redCap = new Product()
+                {
+                    Name = "Red cap",
+                    Price = 8.99M
+                };
+                _context.Add(redCap);
+
+                Product blueTshirt = new Product()
+                {
+                    Name = "Blue t-shirt",
+                    Price = 19.99M
+                };
+                _context.Add(blueTshirt);
+
+                Order order = new Order()
+                {
+                    OrderPlaced = DateTime.Now,
+                    CustomerId = 1
+                };
+
+                Order order2 = new Order()
+                {
+                    OrderPlaced = DateTime.Now.AddDays(-7),
+                    CustomerId = 1
+                };
+
+                _context.Add(order);
+                _context.Add(order2);
+
+                ProductOrder po = new ProductOrder()
+                {
+                    OrderId = 1,
+                    ProductId = 1,
+                    Quantity = 5
+                };
+
+                ProductOrder po2 = new ProductOrder()
+                {
+                    OrderId = 2,
+                    ProductId = 2,
+                    Quantity = 2
+                };
+
+                ProductOrder po3 = new ProductOrder()
+                {
+                    OrderId = 1,
+                    ProductId = 2,
+                    Quantity = 10
+                };
+
+                _context.Add(po);
+                _context.Add(po2);
+                _context.Add(po3);
+
+                _context.SaveChanges();
+            }
+        }
+    }
+}
